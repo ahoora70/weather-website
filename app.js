0bad5168ab63680a24810deb7f7215b4e9da00e5
ahoora70 <ahoora2050@gmail.com>
2019-08-14 16:54:58 +0430

    setup2

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const forecast = require("./utils/forecast");
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 const path = require("path");
 
 app.set("view engine", "hbs");
@@ -20,6 +18,8 @@ app.set("views", viewsPath);
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 //
+
+const port = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
   res.render("index");
